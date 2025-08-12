56c709ea562d4a1452d8299cad928c3925b3ad80
redystum <rffarffarffa@gmail.com>
2025-08-12 23:43:26 +0100

    add button moved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
     <div class="min-h-screen bg-neutral-50 dark:bg-neutral-900 transition-colors duration-200">
         <div class="container mx-auto px-4 py-8 grid gap-y-8">
 
-            <livewire:anime-list list_name="{{$Anime::LIST_WATCHING}}" icon="eye" title="Watching now"
+            <livewire:anime-list list_name="{{$Anime::LIST_WATCHING}}" icon="eye" title="Watching now"/>
+            <livewire:anime-list list_name="{{$Anime::LIST_WATCH}}" icon="tv" title="To Watch"
                                  :haveAdd="true"/>
-            <livewire:anime-list list_name="{{$Anime::LIST_WATCH}}" icon="tv" title="To Watch"/>
             <livewire:anime-list list_name="{{$Anime::LIST_WATCHED}}" icon="check" title="Already Watched"/>
             <livewire:anime-list list_name="{{$Anime::LIST_FAVORITE}}" icon="heart" title="Favorites"/>
 
